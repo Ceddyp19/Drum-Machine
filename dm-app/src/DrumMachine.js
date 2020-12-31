0a05ff29dca4559e46202bae64a0a07cbed1edca
Ceddyp19 <ceddyp117@gmail.com>
2020-12-30 23:09:50 -0600

    created Drum Machine Component'onent

---
+++ dm-app/src/DrumMachine.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+class DrumMachine extends Component {
+    state = {  }
+    render() { 
+        return ( <>
+        DrumMachine
+        </>  );
+
+    }
+}
+ 
+export default DrumMachine
