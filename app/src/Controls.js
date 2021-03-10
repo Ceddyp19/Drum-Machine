936e73ac565565e5099792cba18d11190ef5b530
Ceddyp19 <ceddyp117@gmail.com>
2021-03-10 13:00:07 -0600

    commented out none working buttons for now

--- app/src/Controls.js
+++ app/src/Controls.js
@@ -1,23 +1,26 @@
 const Controls = (props) => {
     return (
         <div id='controller' className='container'>
-            <div className='control'>
+
+            {/* Code commented out because controls aren't working yet */}
+
+            {/* <div className='control'>
                 <p>Power</p>
                 <div className='select'>
                     <div className='inner'></div>
                 </div>
-            </div>
+            </div> */}
             <p id='display'>{props.text}</p>
-            <div className='volume-slider'>
+            {/* <div className='volume-slider'>
                 <input type='range' step='0.01' min='0' max='1' value='.25' />
-            </div>
-            <div className='control'>
+            </div> */}
+            {/* <div className='control'>
                 <p>Bank</p>
                 <div className='select'>
                     <div className='inner'></div>
                 </div>
 
-            </div>
+            </div> */}
         </div>
     )
 }
